b1c2023e17ccfe1a51912295d8c5514089653a43
vvs79 <valentyn-vvs@rambler.ru>
2016-09-12 16:36:42 +0300

    hr

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911150554) do
+ActiveRecord::Schema.define(version: 20160912133510) do
 
   create_table "communications", force: :cascade do |t|
     t.date     "date"
